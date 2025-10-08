b33d834a1fc1fbc7a1105a6c1e66b55d37fbd88f
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2025-10-08 14:52:11 -0400

    Update text-adv.js

--- text-adv.js
+++ text-adv.js
@@ -1,1 +1,9 @@
-
+document.getElementById("input_field").addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    
+    let input = document.forms["input"]["input_field"].value;
+    
+    document.forms["input"]["input_field"].value = "";
+  }
+}, true);
