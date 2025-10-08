e4089b95cf711f9f96e7df6b3f5630ed5080e48c
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2025-10-08 15:19:44 -0400

    Update text-adv.js, added output

--- text-adv.js
+++ text-adv.js
@@ -1,9 +1,15 @@
+let input;
+let output_log;
 document.getElementById("input_field").addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    
-    let input = document.forms["input"]["input_field"].value;
-    
+    input = document.forms["input"]["input_field"].value;
     document.forms["input"]["input_field"].value = "";
+    
+    //output
+    output = input
+    output_log = output + "<br>" + output_log
+    document.getElementById("output").innerHTML = output_log;
   }
 }, true);
+
