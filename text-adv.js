d837240930c96f90ea3bc23fd06d14f6d3553e87
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2025-10-21 13:26:34 -0400

    added rooms

--- text-adv.js
+++ text-adv.js
@@ -1,29 +1,33 @@
-let output_log = "";
-
-// Class definition
-class Room {
-  constructor(x, y, dialogue, items) {
+//Room definition
+class room {
+  constructor(x, y, floor, dialogue, items) {
     this.x = x; // property
     this.y = y; // property
     this.dialogue = dialogue; //property
   }
-
   // methods
   search() {
-    
   }
   loot() {
   }
 }
 
+new const r_gate = new room(0,-3, 1);
+new const r_path = new room(0,-2, 1);
+new const r_door = new room(0,-1, 1);
+new const r_mainRoom = new room(0, 0, 1);
+new const r_mainStairs1 = new room(0, 1, 1);
+new const r_mainStairs2 = new room(0, 1, 2);
+new const r_frontDesk = new room(-1, 0, 1);
+new const r_closet = new room(-2, 0, 1);
+new const r_secretRoom = new room(-1, 0, 1);
+
+let output_log = "";
 document.getElementById("prompt_input").addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
     const input = document.getElementById("prompt_input").value;
     document.getElementById("prompt_input").value = "";
-
-    
-    
     //output
     output = input + "(output)";
     output_log = output_log + "<br>" + output;
