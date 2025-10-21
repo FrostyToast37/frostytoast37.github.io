0b627cb63b7707ce31551c6b13c669df3ee54f34
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2025-10-21 13:29:23 -0400

    Update text-adv.js

--- text-adv.js
+++ text-adv.js
@@ -16,11 +16,16 @@ new const r_gate = new room(0,-3, 1);
 new const r_path = new room(0,-2, 1);
 new const r_door = new room(0,-1, 1);
 new const r_mainRoom = new room(0, 0, 1);
-new const r_mainStairs1 = new room(0, 1, 1);
-new const r_mainStairs2 = new room(0, 1, 2);
+new const r_mainStairsZ1 = new room(0, 1, 1);
+new const r_mainStairsZ2 = new room(0, 1, 2);
 new const r_frontDesk = new room(-1, 0, 1);
-new const r_closet = new room(-2, 0, 1);
-new const r_secretRoom = new room(-1, 0, 1);
+new const r_deskCloset = new room(-2, 0, 1);
+new const r_secretRoom = new room(-3, 0, 1);
+new const r_hallwayX1Y0 = new room(1, 0, 1);
+new const r_hallwayX2Y0 = new room(2, 0, 1);
+new const r_diningRoom = new room(3, 0, 1);
+new const r_kitchen = new room(4, 0, 1);
+
 
 let output_log = "";
 document.getElementById("prompt_input").addEventListener("keypress", function(event) {
