c1d678aa9d4bccafc75de7d185623cf7968a1553
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2025-10-23 12:25:50 -0400

    added move() function

--- text-adv.js
+++ text-adv.js
@@ -1,12 +1,9 @@
-//directions
-const N = "north", S = "south", E = "east", W = "west";
-
 //Room definition
 class room {
   constructor(x, y, floor, exits [], dialogue = "", items = []) {
     this.x = x;
     this.y = y;
-    this.floor = floor;
+    this.z = floor - 1;
     this.exits = exits;
     this.dialogue = dialogue; //property
     this.items = items;
@@ -14,6 +11,19 @@ class room {
   // methods
   search() {}
   loot() {}
+}
+let changes = [];
+function move(room, direction) {
+  for (let i = 0; i < room.exits.length; i++) {
+    if (room.exits[i] == direction) {
+      if (direction == "N"){changes = [0,1,0]}
+      if (direction == "E"){changes = [1,0,0]}
+      if (direction == "S"){changes = [0,-1,0]}
+      if (direction == "W"){changes = [-1,0,0]}
+      let newroom = map[room.x + changes[0]][room.y + changes[1]][room.z + changes[2]];
+      return newroom;
+    }
+  }
 }
 
 //create 3d array to store rooms
@@ -31,30 +41,30 @@ for (let x = 0; x < length; x++) {
 
 //Room Object creations
 //ground floor (z = 1)
-const r_gate =           new room(4, 0, 1, [N]);
-const r_path =           new room(4, 1, 1, [N,S]);
-const r_door =           new room(4, 2, 1, [N,S]);
-const r_mainRoom =       new room(4, 3, 1, [N,E,S,W]);
-const r_mainStairsZ1 =   new room(4, 4, 1, [S]);
-const r_frontDesk =      new room(3, 3, 1, [N,E,W]);
-const r_deskCloset =     new room(2, 3, 1, [E,W]);
-const r_secretRoom =     new room(1, 3, 1, [E]);
-const r_h_X1Y0 =         new room(5, 3, 1, [E,W]);
-const r_h_X2Y0 =         new room(6, 3, 1, [N,E,W]);
-const r_diningRoom =     new room(7, 3, 1, [E,W]);
-const r_kitchen =        new room(8, 3, 1, [N,W]);
-const r_pantry =         new room(8, 4, 1, [S]);
-const r_h_X2Y1 =         new room(6, 4, 1, [N,S]);
-const r_lounge =         new room(7, 4, 1, [E,S]);
-const r_poolRoom =       new room(7, 5, 1, [W]);
-const r_h_Xn1Y1 =        new room(3, 4, 1, [S,W]);
-const r_h_Xn2Y1 =        new room(2, 4, 1, [E,W]);
-const r_h_Xn3Y1 =        new room(1, 4, 1, [E,W]);
-const r_greenhouse =     new room(0, 4, 1, [E,W]);
+const r_gate =           new room(4, 0, 1, ["N"]);
+const r_path =           new room(4, 1, 1, ["N","S"]);
+const r_door =           new room(4, 2, 1, ["N","S"]);
+const r_mainRoom =       new room(4, 3, 1, ["N","E","S","W"]);
+const r_mainStairsZ1 =   new room(4, 4, 1, ["S"]);
+const r_frontDesk =      new room(3, 3, 1, ["N","E","W"]);
+const r_deskCloset =     new room(2, 3, 1, ["E","W"]);
+const r_secretRoom =     new room(1, 3, 1, ["E"]);
+const r_h_X1Y0 =         new room(5, 3, 1, ["E","W"]);
+const r_h_X2Y0 =         new room(6, 3, 1, ["N","E","W"]);
+const r_diningRoom =     new room(7, 3, 1, ["E","W"]);
+const r_kitchen =        new room(8, 3, 1, ["N","W"]);
+const r_pantry =         new room(8, 4, 1, ["S"]);
+const r_h_X2Y1 =         new room(6, 4, 1, ["N","S"]);
+const r_lounge =         new room(7, 4, 1, ["E","S"]);
+const r_poolRoom =       new room(7, 5, 1, ["W"]);
+const r_h_Xn1Y1 =        new room(3, 4, 1, ["S","W"]);
+const r_h_Xn2Y1 =        new room(2, 4, 1, ["E","W"]);
+const r_h_Xn3Y1 =        new room(1, 4, 1, ["E","W"]);
+const r_greenhouse =     new room(0, 4, 1, ["E","W"]);
 
 //upper floor (z = 2)
-const r_mainStairsZ2 =   new room(4, 4, 2, [N]);
-const r_h_X2Y1Z2 =       new room(4, 5, 2, [N,S]);
+const r_mainStairsZ2 =   new room(4, 4, 2, ["N"]);
+const r_h_X2Y1Z2 =       new room(4, 5, 2, ["N","S"]);
 
 //list of rooms
 const rooms = [
@@ -67,7 +77,7 @@ const rooms = [
 ];
 //fill map with rooms
 for (const roomObj of rooms) {
-  map[roomObj.x][roomObj.y][roomObj.floor - 1] = roomObj;
+  map[roomObj.x][roomObj.y][roomObj.z] = roomObj;
 }
   
 //TERMINAL SCRIPTING
