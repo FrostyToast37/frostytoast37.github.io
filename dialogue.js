5a0498ca83ee23ec792888d24475655300326aed
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2025-10-30 13:42:01 -0400

    Create dialogue.js

---
+++ dialogue.js
@@ -0,0 +1,7 @@
+//var room_d = "";
+
+var gate_d = "You see an old, twisted, metal gate, from which, a winding path leads north.";
+var path_d = "Along the old dirt path you spy the door to a giant mansion to the north through the fog.";
+var door_d = "You encounter a massive set of oak doors with a sign above them saying 'The Hydrangea Hotel'. However, spraypainted all around the doors are warnings not to go in. Do you proceed north through the doors?";
+var mainRoom_d = "A gargantuan main lobby welcomes you in. There is a welcome desk to the west, a stairway to the north, and a hallway to the east.";
+var mainStairsZ1_d = "Large wooden stairs loom, leading up to the next floor, and tinted windows cover the walls.";
