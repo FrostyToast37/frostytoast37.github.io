54ca0d9076c918db71542b2d9df3a0471995acd9
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2025-11-10 09:08:50 -0500

    fix devtools

--- text-adv.js
+++ text-adv.js
@@ -323,15 +323,14 @@ document.getElementById("prompt_input").addEventListener("keypress",
         }
           //devtools commands
       } else if (/^D /.test(input) && devtools == true) {
-        if (/^D TP \((\d+)\,(\d+)\,(\d+)\)$/.test(input)) {
+        if (/^\/TP \((\d+)\,(\d+)\,(\d+)\)$/.test(input)) {
           let match = input.match(/^D TP \((\d+),(\d+),(\d+)\)$/);
           currentRoom = map[parseInt(match[1])][parseInt(match[2])][parseInt(match[3])];
           rawOutput = currentRoom.dialogue;
-        } else if (/^D RUN (.+)$/.test(input)) {
+        } else if (/^\/RUN (.+)$/.test(input)) {
           let match = input.match(/^D RUN (.+)$/);
-          function inputFunc(code) {eval(code);}
-          rawOutput = inputFunc(match[1]);
-
+          rawOutput = eval(match[1]);
+          
         } else {
           rawOutput = "dev command invalid";
         }
