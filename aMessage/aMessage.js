77a129c51c276325a5e4f1c268875a4877824de2
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2025-12-17 14:47:21 -0500

    I wonder if this will work.... doubt it

--- aMessage/aMessage.js
+++ aMessage/aMessage.js
@@ -2,6 +2,8 @@
 const loginForm = document.getElementById("login_form");
 const usernameInput = document.getElementById("username_input");
 const passwordInput = document.getElementById("password_input");
+const outputDiv = document.getElementById("output_div");
+let tempJSON  = fetch("/logins.json");
 
 class AccountInfo {
   constructor(password, username) {
@@ -10,11 +12,21 @@ class AccountInfo {
   }
 }
 
+let accountList = JSON.parse(tempJSON)
 
 loginForm.addEventListener("submit", function (event){
   event.preventDefault();
   let tempUser = usernameInput.value;
   let tempPass = passwordInput.value;
 
-  let tempJSON = fetch("/logins.json")
+  let outputFlag;
+  listCheck: for (const AccountObj of accountList) {
+    if (AccountObj.username === tempUser) {
+      if (AccountObj.password === tempPass) {
+        outputFlag = "Logged In";
+        break listCheck;
+      }  else {outputFlag = "Wrong Password";}
+    } else {outputFlag = "That Username doesn't exist";}
+  }
+  outputDiv.innerHTML = outputFlag;
 })
