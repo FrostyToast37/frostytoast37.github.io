da574c9c40219d6d8cf63e6a8ba90667fb3b5d47
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2026-01-22 13:16:53 -0500

    table creation in sql setup

--- aMessage/sqlsetup.js
+++ aMessage/sqlsetup.js
@@ -22,3 +22,11 @@ let con2 = mysql.createConnection({
   database: "mydb"
 });
 
+con2.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con2.query("CREATE TABLE mytable", function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+});
