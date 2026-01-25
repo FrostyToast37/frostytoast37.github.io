8f79b3b89dc4cfa5b259cf42506233c827b43197
FrostyToast37 <andrew.j.morelli@gmail.com>
2026-01-24 20:39:17 -0800

    maybe fixed server compatibility and add express

--- aMessage/client-sign-up.js
+++ aMessage/client-sign-up.js
@@ -2,3 +2,21 @@ const signUpForm = document.getElementById("sign-up-form");
 const usernameInput = document.getElementById("username_input");
 const passwordInput = document.getElementById("password_input");
 const outputDiv = document.getElementById("output_div");
+
+fetch("/register", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    user: "alice",
+    password: "1234"
+  })
+});
+
+fetch("https://frostytoast37.cookiechaos.xyz/aMessage/server-sign-up.js", {
+  method: "POST",
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify({
+    user: "john",
+    password: "secret123"
+  })
+});
--- aMessage/server-sign-up.js
+++ aMessage/server-sign-up.js
@@ -1,8 +1,25 @@
 //imports
 const fs = require("fs");
+const express = require("express");
 let mysql = require("mysql");
 let bcrypt = require("bcrypt");
-let mysql = require('mysql');
+
+class AccountInfo {
+  constructor(password, username) {
+    this.password = password;
+    this.username = username;
+  }
+}
+
+async function getUserInfo() {
+  try {
+    const response = await fetch("https://frostytoast37.cookiechaos.xyz/aMessage/login.html");
+    const data = await response.json();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 
 //connect to sql database
 let con = mysql.createConnection({
@@ -12,12 +29,11 @@ let con = mysql.createConnection({
   database: "mydb"
 });
 
-
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  let sql = "INSERT INTO customers (user, password) VALUES ('', '')";
-  con.query(sql, function (err, result) {
+  let sql = "INSERT INTO customers (user, password) VALUES (?, ?)";
+  con.query(sql, [insertUser, insertPassword], function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   });
