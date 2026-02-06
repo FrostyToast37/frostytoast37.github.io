829c2f64f7e43d775bbafb66e66370a132f3cfb9
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2026-02-06 10:42:26 -0500

    hopefully mpa json to objects

--- periodic-table/ptable.js
+++ periodic-table/ptable.js
@@ -1,1 +1,25 @@
-tableDiv = document.getElementById("table")
+tableDiv = document.getElementById("table");
+
+async function loadJSON() {
+  try {
+    // get file
+    const data = await fetch("/elements.json");
+
+    // parse
+    const parsedJSON = await data.json();
+
+    return parsedJSON;
+
+  } catch (err) {
+    console.error("Error loading file:", err);
+  }
+}
+
+class Element {
+  constructor(data) {
+    //assign properties to objects
+    Object.assign(this, data); 
+  }
+}
+
+const elementsList = loadJSON().map(jsonItem => new Element(jsonItem));
