af97a6adcc0320114fe10e5da231f68c4b9d2735
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2026-03-27 18:38:41 +0000

    its on http

--- node/server.js
+++ node/server.js
@@ -24,7 +24,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: true, 
+    secure: "auto",
     httpOnly: true, 
     sameSite: "lax", 
     maxAge: 1000 * 60 * 60 * 24 //expires in 24 hours
