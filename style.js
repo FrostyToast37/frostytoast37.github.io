e4c9994947b67661b276edf1b5265040951e6a31
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2026-04-07 11:09:44 -0400

    I see

--- style.js
+++ style.js
@@ -4,10 +4,4 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("open");
 });
 
-body.innerHTML += 
-<table>
-  <td id="home">Home</td>
-  <td id="sign-up">Sign up</td>
-  <td id="login">Login</td>
-  <td id="log-out">Log Out</td>
-</table>;
+menu.innerHTML += '<table> <td id="home">Home</td> <td id="sign-up">Sign up</td> <td id="login">Login</td> <td id="log-out">Log Out</td> </table>';
