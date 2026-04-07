f921df2cd059bd72abb52ab7e2c9a89b62d7d575
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2026-04-07 11:00:17 -0400

    menu

--- style.js
+++ style.js
@@ -3,3 +3,11 @@ let menu = document.getElementById("nav-icon3");
 menu.addEventListener("click", () => {
   menu.classList.toggle("open");
 });
+
+menu.innerHTML += 
+<table>
+  <td id="home">Home</td>
+  <td id="sign-up">Sign up</td>
+  <td id="login">Login</td>
+  <td id="log-out">Log Out</td>
+</table>
