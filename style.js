ce069cbaba55b0e02f1352d324c1acb86f0a0f25
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2026-04-07 11:11:49 -0400

    rows

--- style.js
+++ style.js
@@ -4,4 +4,4 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("open");
 });
 
-menu.innerHTML += '<table> <td id="home">Home</td> <td id="sign-up">Sign up</td> <td id="login">Login</td> <td id="log-out">Log Out</td> </table>';
+menu.innerHTML += '<table> <tr><td id="home">Home</td></tr> <tr><td id="sign-up">Sign up</td></tr> <tr><td id="login">Login</td></tr> <tr><td id="log-out">Log Out</td></tr> </table>';
