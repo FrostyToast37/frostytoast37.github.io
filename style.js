85e195af23fe27f508bcc9720f4aad7f396724ca
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2026-04-08 09:36:07 -0400

    logout?

--- style.js
+++ style.js
@@ -1,6 +1,6 @@
 let menu = document.getElementById("drop-menu");
 
-menu.insertAdjacentHTML('beforeend', '<div id="menu-list"><table> <tr><td id="home"><a href="/index.html">Home<a/></td></tr> <tr><td id="sign-up"><a href="/aMessage/sign-up.html">Sign Up<a/></td></tr> <tr><td id="login"><a href="/aMessage/login.html">Login<a/></td></tr> <tr><td id="log-out"><a href="/aMessage/log-out.html">Log Out<a/></td></tr> </table></div>');
+menu.insertAdjacentHTML('beforeend', '<div id="menu-list"><table> <tr><td id="home"><a href="/index.html">Home<a/></td></tr> <tr><td id="sign-up"><a href="/aMessage/sign-up.html">Sign Up<a/></td></tr> <tr><td id="login"><a href="/aMessage/login.html">Login<a/></td></tr> <tr><td id="log-out"><a href="/aMessage/logout.html">Log Out<a/></td></tr> </table></div>');
 
 let icon = document.getElementById("nav-icon3");
 let menuList = document.getElementById("menu-list");
