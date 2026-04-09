16b46b9ed28b3f1a05bf5d366ffc8f957b1c2f09
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2026-04-09 11:33:31 -0400

    ptable js

--- periodic-table/ptable.js
+++ periodic-table/ptable.js
@@ -24,3 +24,27 @@ class Element {
 
 const elementsList = loadJSON().map(jsonItem => new Element(jsonItem));
 
+let table = [];
+
+for (let period = 0; period <= 10; period++) {
+  for (let group = 0; group <= 18; group++) {
+    table[group][period] = elementsList.find(element => element.xpos == group && element.ypos == period);
+  }
+}
+
+let tableHTML = '<table id="ptable">';
+
+// Generate the table rows and cells
+for (let period = 0; period <= 10; period++) {
+  tableHTML += '<tr>';
+  for (let group = 0; group <= 18; group++) {
+    // Use unique IDs for each cell using concatenation
+    tableHTML += `<td id="${table[group][period].name || null}" class="element"><div class="symbol">${table[group][period].symbol || " "}</div></td>`;
+  }
+  tableHTML += '</tr>';
+}
+tableHTML += '</table>';
+
+// Append the generated table to the body
+tableDiv.innerHTML = tableHTML;
+
