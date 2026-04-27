99b34fcf0023e4ee845fe35a6d44488104ce1e90
FrostyToast37 <125480422+FrostyToast37@users.noreply.github.com>
2026-04-27 12:00:13 -0400

    emergency commit because I scared myself

--- node/main.js
+++ node/main.js
@@ -1,9 +1,15 @@
 let test = document.getElementById("test");
-const form = document.getElementById("chat_form");
+const form = document.getElementById("chatdocumnet_form");
 const input = document.getElementById("chat_input");
+
+const recievedMessages = document.getElementById("received_messages");
+const dmForm = document.getElementById("dm_form");
+const dmAddress = document.getElementById("dm_address");
+const dmContent = document.getElementById("dm_content");
 
 const socket = io();
 
+//testing
 async function testFunc() {
   try {
     const res = await fetch("/api/aMessage/main", {
@@ -18,17 +24,33 @@ async function testFunc() {
       console.error(err);
   }
 }
-
 function sendChat(message) {
   socket.emit("msg", message);
 }
-
-
 socket.on("rsp", (res) => {
   test.innerText = res; // world
 });
+
+//dms
+  //senders func
+  function sendDM(to, content) {
+    const data = { to, content };
+    socket.emit("send_dm", data);
+  }
+  function readDM(ID, to) {
+    const data = { ID, to }
+    socket.emit("read", data);
+  }
+
+  //listeners
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   sendChat(input.value);
 })
+
+dmForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendDM(dmAddress.value, dmContent.value);
+});
