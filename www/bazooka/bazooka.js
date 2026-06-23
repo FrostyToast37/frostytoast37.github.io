d93831f93d1701a850c3c4f591902fdb4147ddbf
FrostyToast37 <andrew.morellis22@orange.k12.nc.us>
2026-06-23 09:25:39 -0400

    small tweaks again

--- www/bazooka/bazooka.js
+++ www/bazooka/bazooka.js
@@ -1,134 +1,135 @@
 //DOM elements
-const canvasDOM = document.getElementById("myCanvas");
-canvasDOM.width = window.innerWidth;
-canvasDOM.height = window.innerHeight;
+	const canvasDOM = document.getElementById("myCanvas");
+	canvasDOM.width = window.innerWidth;
+	canvasDOM.height = window.innerHeight;
 
 //game constants (marked by k_)
-const k_collisionEnergy = 0.5; //1=perfectly elastic
-const k_maxSpeed = 18;
-const k_jumpHeight = 35;
-const k_speedConst = 2;
-const k_friction = 0.87;
-const k_laserSpeed = 25; //starting this slow for testing purposes
-const k_laserLength = 50;
-const k_bulletSpeed = 25; //idk why this has to be so much faster
-const g = 3;
+	const k_collisionEnergy = 0.5; //1=perfectly elastic
+	const k_maxSpeed = 18;
+	const k_jumpHeight = 35;
+	const k_speedConst = 2;
+	const k_friction = 0.87;
+	const k_laserSpeed = 25; //starting this slow for testing purposes
+	const k_laserLength = 50;
+	const k_bulletSpeed = 35;
+	const k_bulletGrav = 0.2;
+	const g = 3;
 
 //global declerations
 	let lastTime = 0;
 	let activeAmmo = [];
 	let selectedAmmo = "lasers";
 
-class Laser {
-	constructor(x1, y1, x2, y2) {
-		//starting coords and destination coords
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
+//ammo classes
+	class Laser {
+		constructor(x1, y1, x2, y2) {
+			//starting coords and destination coords
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
 
-		//calculating unit vectors
-		//distances
-		const dx = x2 - x1;
-		const dy = y2 - y1;
-		//magnitude
-		this.magV = Math.sqrt(dx * dx + dy * dy);
-		//unit vectors themselves
-		this.ux = dx / this.magV;
-		this.uy = dy / this.magV;
+			//calculating unit vectors
+			//distances
+			const dx = x2 - x1;
+			const dy = y2 - y1;
+			//magnitude
+			this.magV = Math.sqrt(dx * dx + dy * dy);
+			//unit vectors themselves
+			this.ux = dx / this.magV;
+			this.uy = dy / this.magV;
 
-		this.dTraveled = 0;
-		this.frame = 0;
-		//coords of front of laser
-		this.Xf = x1;
-		this.Yf = y1;
-		//coords of backk of laser
-		this.Xb = x1;
-		this.Yb = y1;
+			this.dTraveled = 0;
+			this.frame = 0;
+			//coords of front of laser
+			this.Xf = x1;
+			this.Yf = y1;
+			//coords of backk of laser
+			this.Xb = x1;
+			this.Yb = y1;
 
-		this.isActive = true;
-	}
-	draw() {
-		this.canvasLaser = new fabric.Line([this.Xf, this.Yf, this.Xb, this.Yb], {
-				stroke: "red",
-				strokeWidth: 5,
+			this.isActive = true;
+		}
+		draw() {
+			this.canvasLaser = new fabric.Line([this.Xf, this.Yf, this.Xb, this.Yb], {
+					stroke: "red",
+					strokeWidth: 5,
+				});
+			canvas.add(this.canvasLaser);
+		}
+		step(dt) {
+			this.dTraveled += (k_laserSpeed  * (dt * 60));
+			this.Xf = (this.dTraveled * this.ux) + this.x1;
+			this.Yf = (this.dTraveled * this.uy) + this.y1;
+			this.Xb = ((this.dTraveled - k_laserLength) * this.ux) + this.x1;
+			this.Yb = ((this.dTraveled - k_laserLength) * this.uy) + this.y1;
+			this.canvasLaser.set({
+				x1: this.Xf,
+				y1: this.Yf,
+				x2: this.Xb,
+				y2: this.Yb //here!
 			});
-		canvas.add(this.canvasLaser);
+			if (this.Xf < 0 || this.Xf > canvasDOM.width || this.Yf < 0 || this.Yf > canvasDOM.height) {
+				this.isActive = false;
+				canvas.remove(this.canvasLaser);
+			}
+
+		}
 	}
-	step(dt) {
-		this.dTraveled += (k_laserSpeed  * (dt * 60));
-		this.Xf = (this.dTraveled * this.ux) + this.x1;
-		this.Yf = (this.dTraveled * this.uy) + this.y1;
-		this.Xb = ((this.dTraveled - k_laserLength) * this.ux) + this.x1;
-		this.Yb = ((this.dTraveled - k_laserLength) * this.uy) + this.y1;
-		this.canvasLaser.set({
-			x1: this.Xf,
-			y1: this.Yf,
-			x2: this.Xb,
-			y2: this.Yb //here!
-		});
-		if (this.Xf < 0 || this.Xf > canvasDOM.width || this.Yf < 0 || this.Yf > canvasDOM.height) {
-			this.isActive = false;
-			canvas.remove(this.canvasLaser);
+	class Bullet { //fix this
+		constructor(x1, y1, x2, y2) {
+			//starting coords and destination coords
+			this.x1 = x1;
+			this.y1 = y1;
+			
+			//math
+			const mx = x2 - x1;
+			const my = y2 - y1;
+			const myx = Math.sqrt((mx * mx) + (my * my));
+			const ratio = k_bulletSpeed / myx;
+
+			//calculating vectors
+			this.vx = ratio * mx;//velocity in the x direction
+			this.vy = ratio * my;//velocity in the y direction
+
+			//coords of front of laser
+			this.x = x1;
+			this.y = y1;
+
+			this.isActive = true;
 		}
 
+		draw() {
+			this.canvasBullet = new fabric.Circle({
+				radius: 2,
+				fill: "black",
+				stroke: "black",
+				strokeWidth: 2,
+				left: this.x,         
+				top: this.y,		
+				originX: "center", 
+				originY: "center",
+				selectable: false
+			});
+			canvas.add(this.canvasBullet);
+		}
+
+		step(dt) {
+			this.vy += g * k_bulletGrav * dt * 60;
+			this.x += this.vx * dt * 60;
+			this.y += this.vy * dt * 60;
+
+			this.canvasBullet.set({
+				left: this.x,
+				top: this.y
+			});
+
+			if (this.x < 0 || this.x > canvasDOM.width || this.y < 0 || this.y > canvasDOM.height) {
+				this.isActive = false;
+				canvas.remove(this.canvasBullet);
+			}
+		}
 	}
-}
-
-class Bullet { //fix this
-	constructor(x1, y1, x2, y2) {
-		//starting coords and destination coords
-		this.x1 = x1;
-		this.y1 = y1;
-		
-		//math
-		const mx = x2 - x1;
-		const my = y2 - y1;
-		const myx = Math.sqrt((mx * mx) + (my * my));
-		const ratio = k_bulletSpeed / myx;
-
-		//calculating vectors
-		this.vx = ratio * mx;//velocity in the x direction
-		this.vy = ratio * my;//velocity in the y direction
-
-		//coords of front of laser
-		this.x = x1;
-		this.y = y1;
-
-		this.isActive = true;
-	}
-
-	draw() {
-		this.canvasBullet = new fabric.Circle({
-			radius: 2,
-			fill: "black",
-			stroke: "black",
-			strokeWidth: 2,
-			left: this.x,         
-			top: this.y,		
-			originX: "center", 
-			originY: "center",
-			selectable: false
-		});
-		canvas.add(this.canvasBullet);
-	}
-
-	step(dt) {
-		this.vy += g * 0.3 * dt * 60;
-		this.x += this.vx * dt * 60;
-		this.y += this.vy * dt * 60;
-
-		this.canvasBullet.set({
-			left: this.x,
-			top: this.y
-		});
-
-		if (this.x < 0 || this.x > canvasDOM.width || this.y < 0 || this.y > canvasDOM.height) {
-            this.isActive = false;
-            canvas.remove(this.canvasBullet);
-        }
-	}
-}
 
 //CANVAS
 	//helloWorld
@@ -170,7 +171,7 @@ class Bullet { //fix this
 	let mouseY = 0;
 	let grounded = true;
 //an array for all key states
-const keysPressed = {};
+	const keysPressed = {};
 //event listeners
 	window.addEventListener("keydown", (event) => {
 		keysPressed[event.code] = true;
@@ -257,43 +258,41 @@ const keysPressed = {};
 		}
 	}
 //gameloop
+	function animate(currentTime) {
+		//deltatime
+			let dt = (currentTime - lastTime) / 1000;
+			//cap dt to prevent massive jumps if the user switches tabs
+			if (dt > 0.1) dt = 0.1; 
+			lastTime = currentTime;
+		//place to check for key presses
+			if (keysPressed["Digit1"]) {
+				selectedAmmo = "lasers";
+			}
+			if (keysPressed["Digit2"]) {
+				selectedAmmo = "bullets";
+			}
 
-function animate(currentTime) {
-	//deltatime
-		let dt = (currentTime - lastTime) / 1000;
-		//cap dt to prevent massive jumps if the user switches tabs
-		if (dt > 0.1) dt = 0.1; 
-		lastTime = currentTime;
-	//place to check for key presses
-		if (keysPressed["Digit1"]) {
-			selectedAmmo = "lasers";
-		}
-		if (keysPressed["Digit2"]) {
-			selectedAmmo = "bullets";
-		}
-
-	//ammo
-		activeAmmo.forEach(ammo => {
-			ammo.step(dt);
+		//ammo
+			activeAmmo.forEach(ammo => {
+				ammo.step(dt);
+			});
+			activeAmmo = activeAmmo.filter(ammo => ammo.isActive !== false);
+		
+		move(dt);
+		aimLine.set({
+			x1: playerX,
+			y1: playerY,
+			x2: mouseX,
+			y2: mouseY
 		});
-		activeAmmo = activeAmmo.filter(ammo => ammo.isActive !== false);
-	
-	move(dt);
-	aimLine.set({
-		x1: playerX,
-		y1: playerY,
-		x2: mouseX,
-		y2: mouseY
+		aimLine.setCoords();
+		canvas.renderAll();
+
+		//this line means it loops
+		requestAnimationFrame(animate);
+	}
+	//starts loop
+	requestAnimationFrame((time) => {
+		lastTime = time;
+		requestAnimationFrame(animate);
 	});
-	aimLine.setCoords();
-	canvas.renderAll();
-
-	//this line means it loops
-	requestAnimationFrame(animate);
-}
-
-//starts loop
-requestAnimationFrame((time) => {
-	lastTime = time;
-	requestAnimationFrame(animate);
-});
